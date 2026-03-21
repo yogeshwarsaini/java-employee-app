e3c34f5880f55e1e3e9fe87a7a6bc23131adf8e5
yogeshwarsaini <yogismash123@gmail.com>
2026-03-22 00:50:19 +0530

    Fix: Use H2 for tests, skip MySQL dependency in CI

--- src/test/java/employee_app/EmployeeAppApplicationTests.java
+++ src/test/java/employee_app/EmployeeAppApplicationTests.java
@@ -2,12 +2,20 @@ package employee_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect"
+})
 class EmployeeAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
